b2cc059a06bc53aa4ad4770020ca6ef5e72b8696
bassra <bassra@gmail.com>
2022-03-01 23:14:59 +0500

    fix multiple instances

--- src/index.ts
+++ src/index.ts
@@ -69,7 +69,7 @@ const process = async (authorization: Serialize.Authorization) => {
 };
 
 const processor = async (authorization: Serialize.Authorization) => {
-  process(authorization);
+  await process(authorization);
   await wait(BOTS_CONFIG.waitTime);
   processor(authorization);
 };
